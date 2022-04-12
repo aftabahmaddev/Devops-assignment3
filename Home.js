c1d12fae75e2ae5be4d64614e4d53f609dc26e8e
aftabah772 <aftabah772@gmail.com>
2022-04-13 00:37:16 +0500

    Aftab's code

---
+++ Home.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from "react-router-dom"
+const Home =()=> {
+        return (
+          <div style={{backgroundColor:'white'}}>
+          <h1>This is Home</h1>
+        </div>
+          );
+}
+ 
+export default Home;
